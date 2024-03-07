e6bd8198aca51d5a5834a2a8ccad0855ee50de11
Faraz Khan <farazgod1234@gmail.com>
2024-03-07 17:18:53 +0530

    Revert "login register finished."
    This reverts commit 9f43e78ef706593975f7ae10cad5b4e0530eb854.

--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -1,59 +1,12 @@
-import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
-
-
 export default function () {
-
-    const [username, setUsername] = useState(null)
-
-    //This is the header component.
-    //call /profile to check if the token is valid.
-    //if valid, set username.
-    useEffect(() => {
-        const getProfile = async () => {
-            const response = await fetch('/api/profile', {
-                credentials: 'include'
-            })
-            // console.log(response);
-            const userInfo = await response.json();
-            console.log(userInfo);
-            if (response.ok) {
-                setUsername(userInfo.username)
-            }
-        }
-        getProfile()
-    }, [])
-
-    async function logout(){
-        const response = fetch('/api/logout' , {
-            method : 'POST',
-        })
-        setUsername(null)
-    }
-
     return (
         <header>
             <Link to="/" className="logo">MyBlog</Link>
             <nav>
-
-                {username && (
-                    <>
-                        <a>Hi @{username}!</a>
-                        <Link to="/create">Create new</Link>
-                        <a onClick={logout}>Logout</a>
-                    </>
-                )}
-
-
-                {!username && (
-                    <>
-                        <Link to="/login">Login</Link>
-                        <Link to="/register">Register</Link>
-                    </>
-                )}
-
-
+                <Link to="/login">Login</Link>
+                <Link to="/register">Register</Link>
             </nav>
         </header>
     );
--- server/index.js
+++ server/index.js
@@ -2,13 +2,6 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 const mongoose = require('mongoose');
-<<<<<<< HEAD
-=======
-const User = require('./models/User')
-const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
-const cookieParser = require('cookie-parser')
->>>>>>> 653548d (login register finished.)
 require('dotenv').config()
 
 main().catch(err => console.log(err));
@@ -19,7 +12,6 @@ async function main() {
 
 app.use(cors())
 app.use(express.json())
-app.use(cookieParser())
 
 app.get("/test" , (req , res)=>res.send("working"))
 
@@ -28,30 +20,5 @@ app.post("/api/register" , (req , res)=>{
     res.json({requestData : {username , password}})
 })
 
-app.get('/api/profile' , (req , res) =>{
-
-    const {token} = req.cookies;
-    
-    //info is the payload of the token along with 
-    //iat (issued at time)
-    if(token){
-        jwt.verify(token, secret, {}, (err,info) => {
-            if (err) throw err;
-            res.json(info);
-          });
-    }
-    else{
-        res.status(400).json({msg:"no token in cookie"})     
-    }
-
-    // res.json({token})
-})
- 
-app.post('/api/logout' , (req , res)=>{
-    res.cookie('token' , '' , {
-        maxAge : 1,
-    })
-    res.json('cookie deleted')
-})
 
 app.listen(3000)
